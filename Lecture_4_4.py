4e8b4b2b0c9fb47c9877e4383b1deaef4fc0ed60
sanghabahn <80108003+sanghabahn@users.noreply.github.com>
2021-03-13 17:50:33 +0900

    Create Lecture_4_4.py

---
+++ Lecture_4_4.py
@@ -0,0 +1,5 @@
+a = "       NICE/TO/MEET/YOU/!/SANGHYUN/SEO     "
+print(a.strip())
+print(a.strip().replace('/', ' '))
+print(a.strip().replace('/',' ').lower())
+print(a.strip().replace('/',' ').capitalize())
